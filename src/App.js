a38008d6c7f76287d13b684b73546f9193f9fca5
YilinRoux <dianaantoni11110@gmail.com>
2025-04-03 01:42:34 -0600

    first commit

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1>Galeria diana</h1>
+        <h1>cdm-galeriadeimagenes-MJLC-5B</h1>
       </header>
       <main>
         <ImageUploader onUploadSuccess={handleUploadSuccess} />
